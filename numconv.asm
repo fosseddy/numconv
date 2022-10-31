10bae9fa739b9e43f162bc8a6b0f25896dbc2e35
fosseddy <markfosseddy@gmail.com>
2022-10-31 19:00:56 +0500

    simple argc, argv

--- numconv.asm
+++ numconv.asm
@@ -1,17 +1,42 @@
 global _start
-extern print
+extern print, STDERR
 extern exit
 
 section .data
-    STDOUT equ 1
+    LF   equ 10
+    NULL equ 0
 
-    msg db "hello, world", 10, 0
+    usage_msg db "Usage: numconv [OPTION] [VALUE]", LF
+              db "Converts VALUE specified by OPTION into binary, decimal, "
+              db "hex, octal", LF
+              db LF
+              db "OPTION", LF
+              db "  -b binary", LF
+              db "  -d decimal", LF
+              db "  -h hex", LF
+              db "  -o octal", LF, NULL
+
+section .bss
+    argc resq 1
+    argv resq 1
 
 section .text
 _start:
-    mov rdi, STDOUT
-    mov rsi, msg
-    call print
+    pop qword [argc]
+    pop qword [argv]
 
+    cmp qword [argc], 2
+    jl .args_error
+
+    jmp .exit_success
+
+.args_error:
+    mov rdi, STDERR
+    mov rsi, usage_msg
+    call print
+    mov rdi, 1
+    call exit
+
+.exit_success:
     mov rdi, 0
     call exit
