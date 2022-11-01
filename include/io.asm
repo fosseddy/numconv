a2afca5508f242d4ba50a25193c9edec9eed2e92
fosseddy <markfosseddy@gmail.com>
2022-11-01 20:20:29 +0500

    add println

--- include/io.asm
+++ include/io.asm
@@ -1,11 +1,13 @@
-global print
+global print, println
 global STDIN, STDOUT, STDERR
 extern strlen
 
 section .data
-    STDIN  equ 0
+    STDIN equ 0
     STDOUT equ 1
     STDERR equ 2
+
+    new_line db 10, 0
 
 section .text
 ; void print(qword fd, byte *s)
@@ -19,5 +21,14 @@ print:
     mov rax, 1
     mov rdx, r10
     syscall
+
+    ret
+
+; void println(qword fd, byte *s)
+println:
+    call print
+
+    mov rsi, new_line
+    call print
 
     ret
