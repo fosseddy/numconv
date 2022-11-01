2766eeabd2651e2fc471ddaa7bcea5627e6ce35d
fosseddy <markfosseddy@gmail.com>
2022-11-01 14:53:51 +0500

    use loop

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -xe
 
 nasm -f elf64 -g numconv.asm
 
-nasm -f elf64 -g include/std.asm
-nasm -f elf64 -g include/io.asm
-nasm -f elf64 -g include/string.asm
-nasm -f elf64 -g include/strconv.asm
+for f in ./include/*.asm
+do
+    nasm -f elf64 -g $f
+done
 
 ld -o numconv numconv.o include/*.o
 
